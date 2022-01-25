755370a6fc36e51f2421ad1a87961459787f1d8c
Şefik Efe <83399767+f4T1H21@users.noreply.github.com>
2022-01-25 10:41:08 +0300

    Update H_new-driver's-license.py

--- H_new-driver's-license.py
+++ H_new-driver's-license.py
@@ -1,5 +1,5 @@
 """
-Nee Driver's License
+New Driver's License
 
 You have to get a new driver's license and you show up at the office at the same time as 4 other people. The office says that they will see everyone in alphabetical order and it takes 20 minutes for them to process each new license. All of the agents are available now, and they can each see one customer at a time. How long will it take for you to walk out of the office with your new license? 
  
